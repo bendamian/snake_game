ed34786aad918f77bbe6f692d766b69fc23d15c8
bendamian <badamian@gmail.com>
2023-11-23 21:38:14 +0000

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from glob import glob
 from turtle import Screen, Turtle
 from typing import List
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake game")
+screen.tracer(0)
 # TODO :creating  snake body
 x = 0
 square_list = []
@@ -22,10 +24,18 @@ for _ in range(3):
 game_is_on = True
 
 while game_is_on:
-    for square in square_list:
-        square.forward(10)
-        x = int(square_list[0].xcor())
-        if x > 550:
-            game_is_on = False
+    screen.update()
+    time.sleep(0.1)
+    for n in range(len(square_list) - 1, 0, -1):
+        new_x = square_list[n-1].xcor()
+        new_y = square_list[n-1].ycor()
+        square_list[n].goto(new_x, new_y)
+    square_list[0].forward(20)
+    square_list[0].left(90)
+
+    x = int(square_list[0].xcor())
+    if x <= 225:
+        continue
+    game_is_on = False
 
 screen.exitonclick()
