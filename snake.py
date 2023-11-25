bffea54dbee625afbd333ba5d446adc23c77be66
bendamian <badamian@gmail.com>
2023-11-25 22:54:40 +0000

    control the snake

--- snake.py
+++ snake.py
@@ -1,13 +1,18 @@
-from turtle import Screen, Turtle
+from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 Move_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.segment = []
         self.create_snake()
+        self.head = self.segment[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -24,4 +29,20 @@ class Snake:
             new_x = self.segment[n - 1].xcor()
             new_y = self.segment[n - 1].ycor()
             self.segment[n].goto(new_x, new_y)
-        self.segment[0].forward(Move_DISTANCE)
+        self.head.forward(Move_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
