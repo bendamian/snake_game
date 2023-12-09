309d9b1072156dcc7673dee8c7daf4ecf954ff87
bendamian <sridamian@gmail.com>
2023-12-09 17:24:33 +0000

    read and write high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,13 @@
 from turtle import Turtle
 
+import data
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.penup()
         self.color('white')
         self.goto(0, 265)
@@ -22,8 +24,18 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_highscore(self.high_score)
         self.score = 0
         self.display()
+
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write(f"GAME OVER", align='center', font=('Arial', 24, 'normal'))
+    def read_high_score(self):
+        with open("data.txt") as data_hs:
+            hs = int(data_hs.read())
+        return hs
+
+    def write_highscore(self, hs):
+        with open("data.txt",mode="w") as data_hs:
+            data_hs.write(f"{hs}")
